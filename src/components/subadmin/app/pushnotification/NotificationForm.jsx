2027a437ee3f4a0152da7695b3115a7e639aafa4
mathewgarfield <189109425+mathewgarfield@users.noreply.github.com>
2025-11-07 05:35:10 -0800

    update:issues

--- src/components/subadmin/app/pushnotification/NotificationForm.jsx
+++ src/components/subadmin/app/pushnotification/NotificationForm.jsx
@@ -12,7 +12,10 @@ const NotificationForm = ({
 }) => {
   return (
     <div>
-      <h2 className="text-[24px] font-[600] mb-2">Send Notifications</h2>
+      <h2 className="text-[24px] font-[600] ">Send Notifications</h2>
+      <p className="text-gray-600 text-[14px] ">
+        Send a message to all Wait at Home users and subscribers.
+      </p>
       <form onSubmit={handleSubmit}>
         <div className="border w-full mt-4 bg-[#FFFFFF66] rounded-[32px] p-8 space-y-5">
           {/* Title */}
@@ -30,7 +33,8 @@ const NotificationForm = ({
 
           {/* Description */}
           <label className="block mb-1 text-sm font-medium text-[#333] capitalize">
-Message          </label>
+            Message{" "}
+          </label>
           <textarea
             name="description"
             rows={6}
@@ -64,9 +68,9 @@ Message          </label>
               value={values.scheduleAt}
               onChange={handleChange}
               onBlur={handleBlur}
-               min={new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
-    .toISOString()
-    .slice(0, 16)} 
+              min={new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+                .toISOString()
+                .slice(0, 16)}
               className={`w-full rounded-xl px-4 py-3 text-[14px] font-[400] bg-white placeholder:text-[#616161] outline-none border transition
                 ${
                   errors.scheduleAt && touched.scheduleAt
@@ -86,9 +90,7 @@ Message          </label>
             type="submit"
             disabled={loading}
             className={`bg-[#5E2E86] font-[600] text-[16px] w-[220px] h-[48px] text-white rounded-[16px] transition-all duration-300 ${
-              loading
-                ? "opacity-70 cursor-not-allowed"
-                : "hover:bg-[#4a236a]"
+              loading ? "opacity-70 cursor-not-allowed" : "hover:bg-[#4a236a]"
             }`}
           >
             {loading ? "Sending..." : "Push"}
